e4925bd57cd06e411ea307830b3920c50b6ae7f8
Kirill <ejemenski@gmail.com>
2020-01-11 20:11:56 +0300

    remove unused import

--- examples/backstack/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ examples/backstack/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ezhemenski.sansfragment.Navigator
 import com.ezhemenski.sansfragment.Transitions
